5f8fbee9286e7476402ba0fb3c561d60dbe26c55
Kalman Szenes <szenes.kalman@gmail.com>
2025-08-10 11:22:42 +0200

    Add indentation and code folding support for compound scripts

--- queries/folds.scm
+++ queries/folds.scm
@@ -2,4 +2,8 @@
   (input_block)
   (geom_block) 
   (subblock)
+  (compound_script)
+  (compound_step_block)
+  (compound_for_loop)
+  (compound_if_block)
 ] @fold
--- queries/indents.scm
+++ queries/indents.scm
@@ -2,9 +2,20 @@
   (input_block)
   (subblock) 
   (geom_block)
+  (compound_script)
+  (compound_step_block)
+  (compound_for_loop)
+  (compound_if_block)
 ] @indent.begin
 
 [
-  "end"
-  "*"
+  (input_block)
+  (subblock) 
+  (geom_block)
+  (compound_script)
+  (compound_step_block)
+  (compound_for_loop)
+  (compound_if_block)
 ] @indent.end
+
+"*" @indent.end
